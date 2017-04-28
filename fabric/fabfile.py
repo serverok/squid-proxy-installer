0c2a3625b13728acfc4ea59ed52c21ca1f99a9dc
Yujin Boby <admin@hostonnet.com>
2017-04-28 11:52:26 +0530

    updated

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -9,8 +9,8 @@ from fabric.api import *
 def squid(squid_username='sadmin', squid_password='superman123'):
     print("Installing Squid")
     os_name = run("cat /etc/*issue")
-    if not os_name.startswith("Ubuntu 16.04"):
-        print("Script only run on Ubuntu 16.04")
+    if not os_name.startswith("Ubuntu 16"):
+        print("Script only run on Ubuntu 16.04 and 16.10")
         quit()
     run("/usr/bin/wget https://raw.githubusercontent.com/HostOnNet/squid-proxy-installer/master/squid3-install.sh -O squid3-install.sh")
     run("chmod 755 squid3-install.sh")

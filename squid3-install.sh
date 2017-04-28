392eab28e8d1fb51b21561c7b617c7e25c127385
Yujin Boby <admin@hostonnet.com>
2017-04-28 11:51:41 +0530

    squid installer now support Ubuntu 14.04

--- squid3-install.sh
+++ squid3-install.sh
@@ -2,19 +2,36 @@
 
 # Squid Installer
 # Author: https://blog.hostonnet.com
+# Email: admin@hostonnet.com
+# Github: https://github.com/HostOnNet/squid-proxy-installer
 
-/usr/bin/apt update
-/usr/bin/apt -y install apache2-utils squid3
+if cat /etc/*release | grep DISTRIB_DESCRIPTION | grep "Ubuntu 16.04"; then
+    /usr/bin/apt update
+    /usr/bin/apt -y install apache2-utils squid3
+    touch /etc/squid/passwd
+    /bin/rm -f /etc/squid/squid.conf
+    /usr/bin/touch /etc/squid/blacklist.acl
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/hostonnet/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /sbin/iptables-save
+    service squid restart
+    update-rc.d squid defaults
+elif cat /etc/*release | grep DISTRIB_DESCRIPTION | grep "Ubuntu 14.04"; then
+    /usr/bin/apt update
+    /usr/bin/apt -y install apache2-utils squid3
+    touch /etc/squid3/passwd
+    /bin/rm -f /etc/squid3/squid.conf
+    /usr/bin/touch /etc/squid3/blacklist.acl
+    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/hostonnet/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /sbin/iptables-save
+    service squid3 restart
+    #update-rc.d squid3 defaults
+    ln -s /etc/squid3 /etc/squid
+else
+    echo "OS NOT SUPPORTED. Contact admin@hostonnet.com to add support for your OS"
+    exit 1;
+fi
+
 
 #/usr/bin/htpasswd -b -c /etc/squid/passwd USERNAME_HERE PASSWORD_HERE
-touch /etc/squid/passwd
-
-/bin/rm -f /etc/squid/squid.conf
-/usr/bin/touch /etc/squid/blacklist.acl
-/usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/hostonnet/squid-proxy-installer/master/squid.conf
-
-/sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
-/sbin/iptables-save
-
-service squid restart
-update-rc.d squid defaults

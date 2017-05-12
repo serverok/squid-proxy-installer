2b662ba358cf17edd0bee7ac9c6ec7142e6756e7
Yujin Boby <admin@hostonnet.com>
2017-05-12 10:49:11 +0530

    added debian 8 support

--- squid3-install.sh
+++ squid3-install.sh
@@ -5,7 +5,7 @@
 # Email: admin@hostonnet.com
 # Github: https://github.com/HostOnNet/squid-proxy-installer
 
-if cat /etc/*release | grep DISTRIB_DESCRIPTION | grep "Ubuntu 16.04"; then
+if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 16.04"; then
     /usr/bin/apt update
     /usr/bin/apt -y install apache2-utils squid3
     touch /etc/squid/passwd
@@ -26,7 +26,21 @@ elif cat /etc/*release | grep DISTRIB_DESCRIPTION | grep "Ubuntu 14.04"; then
     /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
     /sbin/iptables-save
     service squid3 restart
+    ln -s /etc/squid3 /etc/squid
     #update-rc.d squid3 defaults
+    ln -s /etc/squid3 /etc/squid
+elif cat /etc/os-release | grep PRETTY_NAME | grep "jessie"; then
+    # OS = Debian 8
+    /usr/bin/apt update
+    /usr/bin/apt -y install apache2-utils squid3
+    touch /etc/squid3/passwd
+    /bin/rm -f /etc/squid3/squid.conf
+    /usr/bin/touch /etc/squid3/blacklist.acl
+    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/hostonnet/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /sbin/iptables-save
+    service squid3 restart
+    update-rc.d squid3 defaults
     ln -s /etc/squid3 /etc/squid
 else
     echo "OS NOT SUPPORTED. Contact admin@hostonnet.com to add support for your OS"

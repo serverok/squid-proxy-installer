eee84a0216a20c145c24fd752d74b9fc42786d97
Max Base <MaxBaseCode@Gmail.Com>
2019-06-23 03:34:10 +0430

    Update squid3-install.sh

--- squid3-install.sh
+++ squid3-install.sh
@@ -16,6 +16,17 @@ if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 18.04"; then
     /sbin/iptables-save
     service squid restart
     systemctl enable squid
+elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 18.04.2 LTS"; then
+    /usr/bin/apt update
+    /usr/bin/apt -y install apache2-utils squid3
+    touch /etc/squid/passwd
+    /bin/rm -f /etc/squid/squid.conf
+    /usr/bin/touch /etc/squid/blacklist.acl
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/serverok/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /sbin/iptables-save
+    service squid restart
+    systemctl enable squid
 elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 16.04"; then
     /usr/bin/apt update
     /usr/bin/apt -y install apache2-utils squid3

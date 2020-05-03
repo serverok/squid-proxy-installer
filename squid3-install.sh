ee74c022d3e8d57b5d61f4b44af6741802d0e8a5
Justin <40630518+Juxstin@users.noreply.github.com>
2020-05-03 13:37:20 -0500

    Added 20.04 Support
    Added 20.04 Support

--- squid3-install.sh
+++ squid3-install.sh
@@ -3,7 +3,18 @@
 # Squid Installer
 # Author: https://www.serverOk.in
 # Email: info@serverOk.in
-# Github: https://github.com/serverok/squid-proxy-installer
+# Github: https://github.com/serverok/squid-proxy-
+if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 20.04"; then
+    /usr/bin/apt update
+    /usr/bin/apt -y install apache2-utils squid3
+    touch /etc/squid/passwd
+    /bin/rm -f /etc/squid/squid.conf
+    /usr/bin/touch /etc/squid/blacklist.acl
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/Juxstin/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 7777 -j ACCEPT
+    /sbin/iptables-save
+    service squid restart
+    systemctl enable squid
 if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 19.10"; then
     /usr/bin/apt update
     /usr/bin/apt -y install apache2-utils squid3

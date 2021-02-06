d3dcb122848cc1754737de11dbd75272dd159bb9
Yujin Boby <admin@serverok.in>
2021-02-07 03:07:18 +0530

    added squid-user-add

---
+++ squid-user-add.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+############################################################
+# Squid Proxy Installer
+# Author: Yujin Boby
+# Email: info@serverok.in
+# Github: https://github.com/serverok/squid-proxy-installer/
+# Web: https://serverok.in/squid
+############################################################
+# For paid support, contact
+# https://serverok.in/contact
+############################################################
+
+
+read -e -p "Enter Proxy username: " proxy_username
+
+if [ -f /etc/squid/passwd ]; then
+    /usr/bin/htpasswd /etc/squid/passwd $proxy_username
+else
+    /usr/bin/htpasswd -c /etc/squid/passwd $proxy_username
+fi
+
+if [ ! -f /usr/local/bin/sok-find-os ]; then
+    echo "/usr/local/bin/sok-find-os not found"
+    exit 1
+fi
+
+SOK_OS=$(/usr/local/bin/sok-find-os)
+
+if [ $SOK_OS == "ubuntu2004" ]; then
+    systemctl restart squid
+elif [ $SOK_OS == "ubuntu1804" ]; then
+    systemctl restart squid
+elif [ $SOK_OS == "ubuntu1604" ]; then
+    service squid restart
+elif [ $SOK_OS == "ubuntu1404" ]; then
+    service squid3 restart
+elif [ $SOK_OS == "debian8" ]; then
+    service squid3 restart
+elif [ $SOK_OS == "debian9" ]; then
+    systemctl restart squid
+elif [ $SOK_OS == "debian10" ]; then
+    systemctl restart squid
+elif [ $SOK_OS == "centos7" ]; then
+    systemctl restart squid
+else
+    echo "OS NOT SUPPORTED.\n"
+    echo "Contact admin@serverok.in to add support for your os."
+    exit 1;
+fi

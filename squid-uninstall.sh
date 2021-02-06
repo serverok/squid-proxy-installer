18f4ce52231024cc64c4b6e125265797628ad9be
Yujin Boby <admin@serverok.in>
2021-02-07 02:23:59 +0530

    squid uninstall + sok-find-os added

---
+++ squid-uninstall.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+############################################################
+# Squid Proxy Installer
+# Author: Yujin Boby
+# Email: info@serverok.in
+# Github: https://github.com/serverok/squid-proxy-installer/
+# Web: https://serverok.in/squid
+############################################################
+# For paid support, contact
+# https://serverok.in/contact
+############################################################
+
+if [ ! -f /usr/local/bin/sok-find-os ]; then
+    echo "/usr/local/bin/sok-find-os not found"
+    exit 1
+fi
+
+SOK_OS=$(/usr/local/bin/sok-find-os)
+
+if [ $SOK_OS == "ubuntu2004" ]; then
+    /usr/bin/apt -y remove --purge apache2-utils squid3
+    rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu1804" ]; then
+    /usr/bin/apt -y remove --purge apache2-utils squid3
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu1604" ]; then
+    /usr/bin/apt -y remove --purge apache2-utils squid3
+    /bin/rm -rf /etc/squid3/
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu1404" ]; then
+    /usr/bin/apt remove --purge apache2-utils squid3 -y
+    /bin/rm -rf /etc/squid3/
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "debian8" ]; then
+    echo "Uninstalling Squid Proxy on Debian 8"
+    /usr/bin/apt -y remove --purge squid apache2-utils
+    /bin/rm -rf /etc/squid3/
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "debian9" ]; then
+    echo "Uninstalling Squid Proxy on Debian 9"
+    /usr/bin/apt -y remove --purge squid apache2-utils
+    /bin/rm -rf /etc/squid/
+    /bin/rm -rf /var/spool/squid
+elif [ $SOK_OS == "debian10" ]; then
+    echo "Uninstalling Squid Proxy on Debian 10"
+    /usr/bin/apt -y remove --purge squid apache2-utils squid-common squid-langpack
+    /bin/rm -rf /etc/squid/
+    /bin/rm -rf /var/spool/squid
+elif [ $SOK_OS == "centos7" ]; then
+    yum remove squid httpd-tools -y
+    /bin/rm -rf /etc/squid/
+else
+    echo "OS NOT SUPPORTED.\n"
+    echo "Contact admin@serverok.in to add support for your os."
+    exit 1;
+fi
+
+echo 
+echo 
+echo "Squid Proxy uninstalled."
+echo "Thank you for using serverok.in squid proxy installer"
+echo "If you want to reinstall Squid Proxy Server, check https://serverok.in/squid"
+echo 

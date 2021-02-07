91862ead7861efa37403c93889505c96ced5feb5
Yujin Boby <admin@serverok.in>
2021-02-07 10:05:49 +0530

    don't remove apache utils to avoid apache getting removed if already installed.

--- squid-uninstall.sh
+++ squid-uninstall.sh
@@ -18,36 +18,36 @@ fi
 SOK_OS=$(/usr/local/bin/sok-find-os)
 
 if [ $SOK_OS == "ubuntu2004" ]; then
-    /usr/bin/apt -y remove --purge apache2-utils squid3
+    /usr/bin/apt -y remove --purge squid3
     rm -rf /etc/squid/
 elif [ $SOK_OS == "ubuntu1804" ]; then
-    /usr/bin/apt -y remove --purge apache2-utils squid3
+    /usr/bin/apt -y remove --purge squid3
     /bin/rm -rf /etc/squid/
 elif [ $SOK_OS == "ubuntu1604" ]; then
-    /usr/bin/apt -y remove --purge apache2-utils squid3
+    /usr/bin/apt -y remove --purge squid3
     /bin/rm -rf /etc/squid3/
     /bin/rm -rf /etc/squid/
 elif [ $SOK_OS == "ubuntu1404" ]; then
-    /usr/bin/apt remove --purge apache2-utils squid3 -y
+    /usr/bin/apt remove --purge squid3 -y
     /bin/rm -rf /etc/squid3/
     /bin/rm -rf /etc/squid/
 elif [ $SOK_OS == "debian8" ]; then
     echo "Uninstalling Squid Proxy on Debian 8"
-    /usr/bin/apt -y remove --purge squid apache2-utils
+    /usr/bin/apt -y remove --purge squid
     /bin/rm -rf /etc/squid3/
     /bin/rm -rf /etc/squid/
 elif [ $SOK_OS == "debian9" ]; then
     echo "Uninstalling Squid Proxy on Debian 9"
-    /usr/bin/apt -y remove --purge squid apache2-utils
+    /usr/bin/apt -y remove --purge squid
     /bin/rm -rf /etc/squid/
     /bin/rm -rf /var/spool/squid
 elif [ $SOK_OS == "debian10" ]; then
     echo "Uninstalling Squid Proxy on Debian 10"
-    /usr/bin/apt -y remove --purge squid apache2-utils squid-common squid-langpack
+    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
     /bin/rm -rf /etc/squid/
     /bin/rm -rf /var/spool/squid
 elif [ $SOK_OS == "centos7" ]; then
-    yum remove squid httpd-tools -y
+    yum remove squid -y
     /bin/rm -rf /etc/squid/
 else
     echo "OS NOT SUPPORTED.\n"
